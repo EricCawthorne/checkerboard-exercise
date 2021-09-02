b394287ad715bcf0e7b4d06e162ee05dd1e08fa1
EricCawthorne <cawthorneeric@gmail.com>
2021-09-02 16:49:15 -0600

    Checkerboard 9x9

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+for(let i = 1; i <= 81; i++) {
+    if (i % 2 === 0) {
+        let div = document.createElement('div')
+        document.body.style.width = '100vw'
+    document.body.style.height = '100vh'
+    document.body.appendChild(div)
+    const divStyle = div.style
+    divStyle.width = '11.1vw'
+    divStyle.height = '11.1vw'
+    divStyle.backgroundColor = 'red'
+    divStyle.float = 'left'
+    } else {
+        let div = document.createElement('div')
+        document.body.style.width = '100vw'
+        document.body.style.height = '100vh'
+        document.body.appendChild(div)
+        const divStyle = div.style
+        divStyle.width = '11.1vw'
+        divStyle.height = '11.1vw'
+        divStyle.backgroundColor = 'black'
+        divStyle.float = 'left'
+    }
+}
+
